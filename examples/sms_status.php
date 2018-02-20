89eaf8c6a2439eca3e41530d9ffc3a865fa199a1
Ben Mostafa <ben.mostafa87@gmail.com>
2018-02-20 23:09:21 +1100

    Update sms_status.php

--- examples/sms_status.php
+++ examples/sms_status.php
@@ -1,6 +1,8 @@
 <?php
 require_once "vendor/autoload.php";
 
+use MessageMediaMessagesLib\MessageMediaMessagesClient;
+
 $basicAuthUserName = 'YOUR_API_KEY'; // The username to use with basic authentication
 $basicAuthPassword = 'YOUR_API_SECRET'; // The password to use with basic authentication
 
