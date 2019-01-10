e4fa529cbc9dc023506c1ec520eaa9a0590abdb3
Ibrahim_Tareq <ibrahim.tareq@messagemedia.com.au>
2019-01-10 13:37:25 +1100

    Updated README

--- examples/sms_send.php
+++ examples/sms_send.php
@@ -1,28 +1,38 @@
 <?php
 require_once "vendor/autoload.php";
 
-use MessageMediaMessagesLib\MessageMediaMessagesClient;
-use MessageMediaMessagesLib\APIHelper;
+use MessageMediaMessagesLib\Models;
+use MessageMediaMessagesLib\Exceptions;
 
-$authUserName = 'YOUR_API_KEY'; // The API key to use with basic/HMAC authentication
-$authPassword = 'YOUR_API_SECRET'; // The API secret to use with basic/HMAC authentication
-$useHmacAuthentication = false; // Change to true if you are using HMAC keys
+$basicAuthUserName = 'BasicAuthUserName';
+$basicAuthPassword = 'BasicAuthPassword';
 
-$client = new MessageMediaMessagesLib\MessageMediaMessagesClient($authUserName, $authPassword, $useHmacAuthentication);
+$client = new MessageMediaMessagesLib\MessageMediaMessagesClient($basicAuthUserName, $basicAuthPassword);
 
-$messages = $client->getMessages();
+$messagesController = $client->getMessages();
 
-$bodyValue = '{
-   "messages":[
-      {
-         "content":"My first message",
-         "destination_number":"YOUR_MOBILE_NUMBER"
-      }
-   ]
-}';
+$body = new Models\SendMessagesRequest;
+$body->messages = array();
 
+$body->messages[0] = new Models\Message;
 
-$body = MessageMediaMessagesLib\APIHelper::deserialize($bodyValue);
+$body->messages[0]->callbackUrl = 'https://my.callback.url.com';
+$body->messages[0]->content = 'My first message';
+$body->messages[0]->destinationNumber = '+61491570156';
+$body->messages[0]->deliveryReport = true;
+$body->messages[0]->format = Models\FormatEnum::SMS;
+$body->messages[0]->messageExpiryTimestamp = DateTimeHelper::fromRfc3339DateTime('2016-11-03T11:49:02.807Z');
+$body->messages[0]->metadata = MessageMediaMessagesLib\APIHelper::deserialize('{"key1":"value1","key2":"value2"}');
+$body->messages[0]->scheduled = DateTimeHelper::fromRfc3339DateTime('2016-11-03T11:49:02.807Z');
+$body->messages[0]->sourceNumber = '+61491570157';
+$body->messages[0]->sourceNumberType = Models\SourceNumberTypeEnum::INTERNATIONAL;
 
-$result = $messages->createSendMessages($body);
+try {
+    $result = $messagesController->sendMessages($body);
+    echo $result;
+} catch (Exceptions\SendMessages400Response $e) {
+    echo 'Caught SendMessages400Response: ',  $e->getMessage(), "\n";
+} catch (MessageMediaMessagesLib\APIException $e) {
+    echo 'Caught APIException: ',  $e->getMessage(), "\n";
+}
 ?>
